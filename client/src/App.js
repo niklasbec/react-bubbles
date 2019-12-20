deec674371c3cd69b530a8ee3a62d0f054ae02f7
niklasbec <niklasbeckerr@gmail.com>
2019-12-20 11:23:45 +0100

    mvp and stretch done

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,49 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from './components/BubblePage'
 
 function App() {
+
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  })
+
+  const [token, setToken] = useState()
+
+  const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+      {...rest}
+      render={props =>
+        localStorage.getItem("token") ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/" />
+        )
+      }
+    />
+  )
+
+  const logout = () => {
+    localStorage.removeItem('token')
+  }
+
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
+        <div className='buttons'>
+        <Link to='/login'>
+        <button>Login</button>
+        </Link>
+        <Link to='/'>
+        <button className='logout' onClick={logout}>Logout</button>
+        </Link>
+        </div>
+        <PrivateRoute path='/bubbles' component={BubblePage} />
+        <Route exact path='/login' render={(props) => <Login {...props} credentials={credentials} setCredentials={setCredentials}  />}/>
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
